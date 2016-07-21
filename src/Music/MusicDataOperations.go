38ca3f7caaa89abdd658b21ca1a76efd11be5a61
Raim Interiano <rai.interiano@gmail.com>
2016-07-21 12:46:23 -0600

    Renaming and separation of files for future work

--- src/Music/MusicDataOperations.go
+++ src/Music/MusicDataOperations.go
@@ -2,14 +2,7 @@ package Music
 
 import (
 	"fmt"
-	"github.com/jmoiron/sqlx" //check alias
+	"github.com/jmoiron/sqlx"
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql" //aliased so we don't depend on driver's functions
 )
-
-type User struct {
-	AlbumId int
-	Title string
-	Price float
-	Rating int
-}
